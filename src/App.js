9c727a6473247be0d1afd35d8188dee5d8cff1b5
Jayson <jaysonluong3@gmail.com>
2024-09-19 23:30:10 -0500

    trying to fix that import error

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import Login from './Pages/Login';
 import AdditionalInfo from './Pages/AdditionalInfo';
 import Navbar from './Navbar';
 import Register from './Pages/Register'
-import VolunteerMatching from './Pages/volunteerMatching'
+import VolunteerMatching from './Pages/VolunteerMatching'
 import Notifications from './Pages/Notifications';
 
 function App() {
